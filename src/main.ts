0a8c9761e45c21e968676afb67f71d1688f181c0
Stefanospedicato <stefano.spedicato11@gmail.com>
2025-04-29 15:31:11 +0200

    feat: snack 3 completed

--- src/main.ts
+++ src/main.ts
@@ -48,7 +48,7 @@ type Dipendente = {
   annoNascita: number,
   sesso: 'm' | 'f',
   anniDiServizio: number[],
-  //emailAziendale: 'Email assegnata al dipendente' as const,
+  //emailAziendale: 'Email assegnata al dipendente',
   contratto: 'indeterminato' | 'determinato' | 'freelance'
 }
 
@@ -62,4 +62,75 @@ const utente: Dipendente = {
   contratto: 'indeterminato'
 }
 
-console.log(utente);
+/*
+Estendiamo Dipendente per definire due ruoli specifici all'interno dell'azienda:
+
+Developer
+livelloEsperienza → Il livello di esperienza del developer (le scelte possibili sono solo “Junior”, “Mid” o “Senior”).
+linguaggi → Un array contenente i linguaggi di programmazione utilizzati dal developer in azienda (opzionale, perché i neo assunti non hanno ancora dei linguaggi assegnati).
+certificazioni → Un array di stringhe contenente certificazioni tecniche ottenute dal developer (può essere vuoto).
+ProjectManager
+teamSize → Il numero di persone nel team gestito dal Project Manager (può essere null se non ha ancora un team assegnato).
+budgetGestito → Il totale del budget annuale gestito dal PM (opzionale).
+stakeholderPrincipali → Un array di stringhe con i nomi dei principali stakeholder con cui il PM collabora (può essere vuoto).
+🎯 BONUS
+Definiamo un nuovo type alias Team, che rappresenta un gruppo di lavoro all'interno dell'azienda:
+
+nome → Nome del team (stringa).
+progettoAttuale → Nome del progetto su cui lavora il team (può essere null se il team è in attesa di assegnazione).
+budget → Importo numerico del budget assegnato al team (sempre presente).
+membri → Una tuple in cui il primo elemento è sempre un Project Manager, seguito da uno o più Developers (almeno un developer obbligatorio).
+*/
+
+type Developer = Dipendente & {
+  livelloEsperienza: 'Junior' | 'Mid' | 'Senior',
+  linguaggi?: string[],
+  certificazioni: string[],
+}
+
+type ProjectManager = Dipendente & {
+  teamSize: number | null,
+  budgetGestito?: number,
+  stakeholderPrincipali: string[],
+}
+
+type Team = {
+  nome: string,
+  progettoAttuale: string | null,
+  budget: number,
+  membri: [ProjectManager, Developer, Developer[]?]
+}
+
+const fabrizio: ProjectManager = {
+  nome: 'Stefano',
+  cognome: 'Spedicato',
+  annoNascita: 1997,
+  sesso: 'm',
+  anniDiServizio: [2024, 2025],
+  //emailAziendale: 'Email assegnata al dipendente' as const,
+  contratto: 'indeterminato',
+  teamSize: 5,
+  budgetGestito: 7000,
+  stakeholderPrincipali: ['string[]'],
+}
+
+const mimmo: Developer = {
+  nome: 'Stefano',
+  cognome: 'Spedicato',
+  annoNascita: 1997,
+  sesso: 'm',
+  anniDiServizio: [2024, 2025],
+  //emailAziendale: 'Email assegnata al dipendente' as const,
+  contratto: 'indeterminato',
+  livelloEsperienza: 'Junior',
+  certificazioni: ['Java'],
+}
+
+const cavalli: Team = {
+  nome: 'i Cavalli',
+  progettoAttuale: 'Nitrito',
+  budget: 2000000,
+  membri: [fabrizio, mimmo]
+}
+
+console.log(cavalli);
